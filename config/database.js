901b13d005b4b1ecc590c3e629b2e378df7330cd
Jonathon Blom <blomjonathon@gmail.com>
2025-05-07 11:24:31 -0500

    deploy to render

--- config/database.js
+++ config/database.js
@@ -4,12 +4,18 @@ require('dotenv').config();
 const sequelize = new Sequelize(
     process.env.DB_NAME || 'invoice_db',
     process.env.DB_USER || 'postgres',
-    process.env.DB_PASSWORD || 'jankcarbon',
+    process.env.DB_PASSWORD,
     {
         host: process.env.DB_HOST || 'localhost',
         port: process.env.DB_PORT || 5432,
         dialect: 'postgres',
         logging: false, // Set to console.log to see SQL queries
+        dialectOptions: {
+            ssl: process.env.NODE_ENV === 'production' ? {
+                require: true,
+                rejectUnauthorized: false
+            } : false
+        },
         pool: {
             max: 5,
             min: 0,
